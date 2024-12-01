cfc09fa5d9d438398d6bd209c6f2de759d585582
yadavadarsh55 <153019946+yadavadarsh55@users.noreply.github.com>
2024-12-01 21:38:24 +0530

    added the predict image function in app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, redirect,url_for
 from text_model_train import predict_text
-from transformers import TFBertForSequenceClassification, BertTokenizer
+from image_model_train import predict_single_image, preprocess_image
+from transformers import TFBertForSequenceClassification, BertTokenizer, ViTForImageClassification, ViTImageProcessor
 
-model = TFBertForSequenceClassification.from_pretrained("./text_model")
+text_model = TFBertForSequenceClassification.from_pretrained("./text_model")
+image_model = ViTForImageClassification.from_pretrained("./image_model")
+
 tokenizer = BertTokenizer.from_pretrained("./text_model")
+processor = ViTImageProcessor.from_pretrained("./image_model")
 
 app = Flask(__name__)
 
@@ -38,8 +42,25 @@ def checkImage():
 @app.route('/predictText/', methods=['POST'])
 def predictText():
     text = request.form.get('user_text')
-    prediction, confidence = predict_text(text, tokenizer, model)
+    prediction, confidence = predict_text(text, tokenizer, text_model)
     return f"This text is {prediction} with the confidence {confidence:.2f}"
+
+@app.route('/predictImage/', methods=['POST'])
+def predictImage():
+    if 'image' not in request.files:
+        return 'No image file uploaded'
+    image_file = request.files['image']
+    try:
+        inputs = preprocess_image(image_file, processor)
+        predicted_label, confidence = predict_single_image(
+            image_file,
+            image_model,
+            processor,
+            inputs
+        )
+        return f'predicted_label: {predicted_label}, confidence: {confidence}'
+    except Exception as e:
+        return str(e)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
